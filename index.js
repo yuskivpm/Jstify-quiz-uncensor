adfb47e5d0b6fed9426d7029eac810283a539c24
Petro Yuskiv <58133687+yuskivpm@users.noreply.github.com>
2021-02-13 22:20:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const uncensor = (message, vowels, vowelsAsArray = [...vowels]) => message.replace(/\*/g, () => vowelsAsArray.shift());
 
 const uncensor_v2 = (message, vowels) => message.split('*').map((item, index) => `${item}${vowels[index]??''}`).join('');
+
+const uncensor_v3 = (message, vowels, index = 0) => message.replace(/\*/g, () => vowels[index++]);
+
+const uncensor_v4 = (message, vowels) => {
+  let result = '';
+  for (let i = j = 0; i < message.length; i++) {
+    result += message[i] === '*' ? vowels[j++] : message[i];
+  }
+  return result;
+};
